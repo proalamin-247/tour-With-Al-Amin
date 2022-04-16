41ffd6847122278aa811df97e8e5fdf5df6a7139
proalamin-247 <pro.alamin247@gmail.com>
2022-04-17 01:39:46 +0600

    react router install than navbar added and routes setup

---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <>
+            <Navbar collapseOnSelect sticky='top' expand="lg" bg="light" variant="light">
+                <Container>
+                    <Navbar.Brand as={Link} to="/">
+                        Tour W Alamin
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="packages">Packages</Nav.Link>
+                            <Nav.Link href="blogs">Blogs</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link as={Link} to="/about">About</Nav.Link>
+                            {/* {
+                                user ?
+                                    <button className='btn btn-primary' onClick={handaleSignOut}>Sign Out</button>
+                                    : */}
+                                    <Nav.Link eventKey={2} as={Link} to="/login">
+                                        Login
+                                    </Nav.Link>
+                                    {/* } */}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    );
+};
+
+export default Header;
