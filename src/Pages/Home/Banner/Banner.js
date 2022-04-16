091e7c629dd4042b3c5b72a5a8e42d090bd3fe82
proalamin-247 <pro.alamin247@gmail.com>
2022-04-17 02:19:16 +0600

    banner section added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,9 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Carousel } from 'react-bootstrap';
+
+import banner1 from '../../../images/banner/img1.jpg';
+import banner2 from '../../../images/banner/img2.jpg';
+import banner3 from '../../../images/banner/img3.jpg';
 
 const Banner = () => {
+    const [index, setIndex] = useState(0);
+
+    const handleSelect = (selectedIndex, e) => {
+        setIndex(selectedIndex);
+    };
+
     return (
-        <div>
-            <h1>Hi! This is Banner</h1>
+        <div className='container mt-4 '>
+            <Carousel activeIndex={index} onSelect={handleSelect}>
+                <Carousel.Item className='rounded'>
+                    <img
+                        className="d-block w-100 rounded"
+                        src={banner1}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>Sylhet Premium Trip</h3>
+                        <p>It is a 2 days 3 Nights Sylhet Premium Trip</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 rounded"
+                        src={banner2}
+                        alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>St. Martin Economy trip 2D+3N</h3>
+                        <p>It is a St. Martin Economy Package for students specially</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 rounded"
+                        src={banner3}
+                        alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Sajek Premium Trip</h3>
+                        <p>
+                            It is a 2 days 3 Nights Sajek Premium Trip.
+                        </p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
         </div>
     );
 };
--- src/Pages/Home/Packages/Packages.js
+++ src/Pages/Home/Packages/Packages.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Packages = () => {
     return (
         <div>
-            <h2>This is pacakges</h2>
+            <h2>This is</h2>
         </div>
     );
 };
