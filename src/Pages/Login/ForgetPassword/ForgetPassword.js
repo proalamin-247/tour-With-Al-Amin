5f2b2c833cc1f8c5a0af458b297160a2487ccaec
proalamin-247 <pro.alamin247@gmail.com>
2022-04-18 00:35:43 +0600

    forget password page design and get input field

--- src/Pages/Login/ForgetPassword/ForgetPassword.js
+++ src/Pages/Login/ForgetPassword/ForgetPassword.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 const ForgetPassword = () => {
+    const emailRef = useRef('');
+    let errorElement;
+
+    const handaleSubmit = (event) => {
+        event.preventDefault();
+        const email = emailRef.current.value;
+    }
+    const resetPassword = async () => {
+        const email = emailRef.current.value;
+        
+        console.log(email);
+    }
+
     return (
-        <div>
-            <h1>Forget password</h1>
+        <div className='container p-5 w-25 mx-auto mt-5 border rounded'>
+            <h2>Forget Password</h2>
+            <form onSubmit={handaleSubmit} className='mt-2'>
+                <div className="mb-3">
+                    <input ref={emailRef} type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Email' />
+                    <div id="emailHelp" className="form-text">type your account email, which you use create tyour account</div>
+                </div>
+                {errorElement}
+                <button onClick={resetPassword} type="submit" className="btn btn-primary">Submit</button>
+            </form>
         </div>
     );
 };
