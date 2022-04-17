f7b4752cbb4ff1b37402e3ef896da8bbc00ea87c
proalamin-247 <pro.alamin247@gmail.com>
2022-04-17 14:47:02 +0600

    login and register form added and also set up togle route and forget password

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Blogs from './Pages/Blogs/Blogs';
 import Login from './Pages/Login/Login';
 import About from './Pages/About/About';
 import NotFound from './Pages/NotFound/NotFound';
+import Register from './Pages/Login/Register/Register';
+import ForgetPassword from './Pages/Login/ForgetPassword/ForgetPassword';
 
 function App() {
   return (
@@ -19,6 +21,8 @@ function App() {
         <Route path='/blogs' element={<Blogs></Blogs>}></Route>
         <Route path='/about' element={<About></About>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
+        <Route path='/forgetpassword' element={<ForgetPassword></ForgetPassword>}></Route>
         <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -2,13 +2,15 @@ import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { Link, useNavigate } from 'react-router-dom';
 
-const Login = () => {
+const Register = () => {
+    const nameRef = useRef('');
     const emailRef = useRef('');
     const passwordRef = useRef('');
     const navigate = useNavigate();
 
     const handaleSubmit = event => {
         event.preventDefault();
+        const name = nameRef.current.value;
         const email = emailRef.current.value;
         const password = passwordRef.current.value;
 
@@ -18,28 +20,30 @@ const Login = () => {
     }
     return (
         <div className='container w-25 mx-auto mt-5'>
-            <h2 className='text-primary text-center'>Please Login...</h2>
+            <h2 className='text-primary text-center'>Please Register...</h2>
             <Form onSubmit={handaleSubmit}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Your Name</Form.Label>
+                    <Form.Control ref={nameRef} type="email" placeholder="Full name" required />
+                </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
                     <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
                 </Form.Group>
-
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
                     <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
                 </Form.Group>
-                <Link to='/forgetpassword' className='text-primary pe-auto text-decoration-none'>Forget Password</Link>
-                <Button variant="primary w-100 mt-3" type="submit">
-                    Login
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
                 </Button>
-                <p className='mt-2'>Have not account? <Link to='/register' className='text-primary pe-auto text-decoration-none' onClick={navigateRagister}>Please Register</Link></p>
+                <p>Already have an account? <Link to='/login' className='text-danger pe-auto text-decoration-none'>Please Login</Link></p>
             </Form>
         </div>
     );
 };
 
-export default Login;
+export default Register;
