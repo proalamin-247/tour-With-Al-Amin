d0aa95480aba1a3f87760870b02f88950d27c559
proalamin-247 <pro.alamin247@gmail.com>
2022-04-17 16:53:41 +0600

    login and sign out email+password added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,18 +1,40 @@
 import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Login = () => {
     const emailRef = useRef('');
     const passwordRef = useRef('');
     const navigate = useNavigate();
+    const location = useLocation();
+    let from = location.state?.from?.pathname || "/";
+    let errorElement;
+
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
 
     const handaleSubmit = event => {
         event.preventDefault();
         const email = emailRef.current.value;
         const password = passwordRef.current.value;
 
+        signInWithEmailAndPassword(email, password);
     }
+    if (user) {
+        navigate(from, { replace: true });
+    }
+    if (error) {
+        errorElement = (
+            <p className='text-danger'>Error: {error?.message}</p>
+        );
+    }
+
     return (
         <div className='container w-25 mx-auto mt-5'>
             <h2 className='text-primary text-center'>Please Login...</h2>
@@ -24,7 +46,6 @@ const Login = () => {
                         We'll never share your email with anyone else.
                     </Form.Text>
                 </Form.Group>
-
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
                     <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
@@ -35,6 +56,7 @@ const Login = () => {
                 </Button>
                 <p className='mt-2'>Have not account? <Link to='/register' className='text-primary pe-auto text-decoration-none'>Please Register</Link></p>
             </Form>
+            {errorElement}
         </div>
     );
 };
