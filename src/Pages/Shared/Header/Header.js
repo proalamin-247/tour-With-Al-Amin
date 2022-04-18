86683de6f7c5ff99685bd590750b3f8f0902e4b6
proalamin-247 <pro.alamin247@gmail.com>
2022-04-18 15:42:38 +0600

    final check 1.0 (logo name edit in header)

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -15,7 +15,7 @@ const Header = () => {
             <Navbar collapseOnSelect sticky='top' expand="lg" bg="light" variant="light">
                 <Container>
                     <Navbar.Brand as={Link} to="/">
-                        Tour W Alamin
+                        Tour With Alamin
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
