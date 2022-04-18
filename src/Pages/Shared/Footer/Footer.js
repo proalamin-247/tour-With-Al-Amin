55b034678d4d5928a6d7b403bb719be0a3b39a27
proalamin-247 <pro.alamin247@gmail.com>
2022-04-18 15:16:14 +0600

    footer redign and add routes

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,11 +1,37 @@
-// import React from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Footer.css'
 
-export default function Footer() {
+const Footer = () => {
     const today = new Date();
     const year = today.getFullYear();
     return (
-        <div className='text-center mt-5 mb-2'>
-            &copy; {year} Tour With Al-Amin. All rights reserved.
+        <div className='footer'>
+            <div className='d-flex justify-content-around align-items-center p-5'>
+                <div>
+                    <h6>CONTACT INFORMATION</h6>
+                    <p>137/12, B-25, Priangon, Mirpur-1, Dhaka-1216.</p>
+                    <p><span className='fw-bold mb-0'>Phone: </span>01887218**</p>
+                    <p><span className='fw-bold'>Email: </span>admin@tourwithalamin.com</p>
+                </div>
+                <div>
+                    <h6>LEGAL</h6>
+                    <Link to='/terms' className='pe-auto text-white text-decoration-none'>
+                        Terms & Conditions
+                    </Link>
+                </div>
+                <div>
+                    <h6>SITEMAPS</h6>
+                    <Link to='/about' className='pe-auto text-white text-decoration-none'>
+                        About Us
+                    </Link>
+                </div>
+            </div>
+            <div className='text-center '>
+                &copy; {year} <span className='fw-bold'>Tour With Al-Amin</span>. All rights reserved.
+            </div>
         </div>
     );
-}
+};
+
+export default Footer;
